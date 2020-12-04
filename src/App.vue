7f6efd7c8441c59e596b94c80fcaf34a486c4d1d
Drew Harper <druharper@gmail.com>
2020-12-04 15:12:34 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block
+    v-if="isPlaying"
+    :delay="delay"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Block from '@/components/Block'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: {
+      Block
+    },
+    data() {
+      return {
+        isPlaying: false,
+        delay: null
+      }
+    },
+    methods: {
+      start(){
+        this.delay = 2000 + Math.random() * 5000
+        this.isPlaying = true
+        console.log(this.delay)
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="block" v-if="showBlock">
+    Click me!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Block',
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false
+    }
+  },
+  mounted() {
+    console.log("component mounted")
+    setTimeout(() => {
+      this.showBlock = true
+      console.log(this.delay)
+    }, this.delay);
+  },
+  updated() {
+    console.log('component updated')
+  },
+  unmounted(){
+    console.log('component unmounted');
+  }
+}
+</script>
+
+<style>
+  .block {
+    width: 25rem;
+    border-radius: 1rem;
+    background: #0faf87;
+    color: white;
+    text-align: center;
+    padding: 7rem 0;
+    margin: 4rem auto;
+    cursor: pointer;
+  }
+</style>
